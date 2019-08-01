7ebe6c4bb3fe441ea177145179d3fce67d80e8a0
Costin Manolache <costin@gmail.com>
2019-08-01 14:11:36 -0700

    Renamed

--- test/inplace/inplace.mk
+++
@@ -1,7 +0,0 @@
-
-# Installs Istio 1.2 using default install yaml, install canary, verify canary starts.
-test-inplace:
-	kubectl apply -k kustomize/istio-1.2/default --prune -l release=istio
-	kubectl apply -k kustomize/istio-canary --prune -l release=istio-canary
-	kubectl wait deployments istio-pilot-canary istio-galley-canary istio-sidecar-injector-canary -n ${ISTIO_SYSTEM_NS} --for=condition=available --timeout=${WAIT_TIMEOUT}
-

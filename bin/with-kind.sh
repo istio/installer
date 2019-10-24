05264f3dad211bb0ba43a447335dd58e82227098
John Howard <howardjohn@google.com>
2019-10-24 13:00:02 -0700

    or true

--- bin/with-kind.sh
+++ bin/with-kind.sh
@@ -23,7 +23,7 @@ export PATH=${GOPATH}/bin:${PATH}
 
 function cleanup_kind_cluster() {
   if [[ -z "${SKIP_KIND_CLEANUP:-}" ]]; then
-    kind export logs --name istio-testing "${ARTIFACTS}/kind"
+    kind export logs --name istio-testing "${ARTIFACTS}/kind" || true
     kind delete cluster --name=istio-testing
   fi
 }
